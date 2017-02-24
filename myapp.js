c425ba3abc0254952d755eb70878e71bd10c459e
DONKENA SHIVA KUMAR GOUD <DONKENA SHIVA KUMAR GOUD>
2017-02-24 15:16:04 +0530

    new

--- myapp.js
+++ myapp.js
@@ -1,10 +1,20 @@
 var app=angular.module("myapp",[])
-app.controller("mycontroller",function($scope)
+app.controller("mycontroller",function($scope,$http)
 {
   $scope.values;
   $scope.name="shiva";
-var numbers=[{name :"Ramu", age:21,branch:"cse"},
+var numbers=[{name :"raasdsdmu", age:21,branch:"cse"},
 {name :"Ravi", age:21,branch:"cse"},
-{name :"Sampath", age:21,branch:"ecee"}];
+{name :"Samsdspath", age:21,branch:"ecee"}];
 $scope.numbers=numbers;
+var a= function()
+{
+  console.log("sdsd");
+  return "shiva";
+}
+$scope.checked="true";
+$http.get("http://sonutech.comuv.com/signup.php")
+ .then(function(response) {
+     $scope.myWelcome = response.data;
+ });
 });
